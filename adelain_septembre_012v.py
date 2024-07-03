de004c257b0bacf5970a369e3a58ed31c7ce6cc1
princhuaker <167265937+princhuaker@users.noreply.github.com>
2024-07-02 20:40:33 -0400

    s

---
+++ adelain_septembre_012v.py
@@ -0,0 +1,28 @@
+import csv
+import random 
+
+print ("BIVENIDO A CATPREMIUN")
+
+def Menu_registro ():
+    print ("Menu")
+    print ("1. registra pedido")
+    print ("2. lista de todos los pedidos")
+    print ("3. imprimir hoja de ruta")
+    print ("4. salir del programa")
+
+
+def registar_pedido():
+    nombre = input ("ingrese su nombre y apellido:")
+    direccion = input ("ingrese su direccion:")
+    comuna = input ("ingrese la comuna:")
+
+def list_de_pedidos ():
+    print (" ingresa todos los pedidos")
+
+def imprimir_hoja_de_ruta ():
+    print (" ingrese la su ubicacion")
+    print (" san bernado")
+
+def salir_del_programa ():
+    print (" salir del programa")
+
